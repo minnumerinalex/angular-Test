f2c52b8f2a8a001dd989cea566d180d816683a27
minnumerinalex <minnumerinalex@gmail.com>
2021-09-25 07:43:28 +0530

    Input,Output

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child.component';
-import { PatientComponent } from './patient/patient.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, ChildComponent,PatientComponent ],
+  declarations: [ AppComponent, ChildComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
